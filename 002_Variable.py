de17f13ca53a0229b0690fb8ca1d8c37aae6522f
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2024-11-25 03:05:58 +0600

    Create Variable

---
+++ 002_Variable.py
@@ -0,0 +1,10 @@
+age = 25
+Weight = 70.5
+Name = "Hasan Piash"
+Department = "Computer Science"
+is_sigle = True
+is_sleeping = False
+print (age)
+print (age + Weight)
+print (Name , Department)
+print ('Python' + ' ' + 'Django')

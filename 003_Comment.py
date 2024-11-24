05e0a6e3df8d1ccf0bb663306b339e7a6323f96e
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2024-11-25 03:09:22 +0600

    Create Comment

---
+++ 003_Comment.py
@@ -0,0 +1,17 @@
+age = 25
+Weight = 70.5
+Name = "Hasan Piash"
+Department = "Computer Science"
+is_sigle = True
+is_sleeping = False
+print (age)
+print (age + Weight)
+print (Name , Department)
+
+# Single Line Comment : CTRL + /
+# Mark the Line, and Press CTRL + /      or      type # starting of line.
+
+""" 
+For Multiple line comment
+Press : ALT + SHIFT + A 
+"""

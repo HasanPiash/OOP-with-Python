d300a98ca9ad2d7119f9ad301551c07be06d7190
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2024-11-25 03:11:17 +0600

    Create Variable type

---
+++ 004_Find_Variable_Type.py
@@ -0,0 +1,15 @@
+age = 25
+Weight = 70.5
+Name = "Hasan Piash"
+Department = "Computer Science"
+is_sigle = True
+is_sleeping = False
+print (age)
+print (age + Weight)
+print (Name , Department)
+
+# Find the Variable type :
+
+print(type(age)) 
+print(type(Name))
+print(type(Weight))

099ae54ab70bed76e726572629dde7b438e1f6c6
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2024-11-26 17:47:22 +0600

    Create Input
    3 types of input

---
+++ 005_Input.py
@@ -0,0 +1,27 @@
+print ('Input Something')
+Account = input('Give me some Money: ')
+print(Account)
+
+""" 
+OUTPUT: Give me some Money:100
+RESULT: 100 
+"""
+
+First_money = input("Enter First Money: ")
+Second_money = input ("Enter Second Money: ")
+print(First_money + Second_money)
+
+"""
+INPUT : 500 & 500
+OUTPUT : 500500
+"""
+
+# After Typecasting
+First_money = input("Enter First Money: ")
+Second_money = input ("Enter Second Money: ")
+print (int (First_money) + int (Second_money))
+
+""" 
+INPUT: 50 & 50
+OUTPUT: 100
+"""

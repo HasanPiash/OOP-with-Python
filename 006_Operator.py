e19b5833cb93fe3c55b971f6883079f2d542cd59
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2024-11-27 18:28:17 +0600

    Create Operator

---
+++ 006_Operator.py
@@ -0,0 +1,14 @@
+# Operator: +, -, *, /
+num1 = 50
+num2 = 50
+print (num1 + num2)
+print (num1 - num2)
+print (num1 * num2)
+print (num1 / num2)
+
+print (10+5)
+print (10-5)
+print (10*5)
+print (10/3)
+print (10//3)
+print (10%3) 

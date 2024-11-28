4c27c4a46a6a83a19249a00ac28a021dcdd997ef
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2024-11-28 18:06:55 +0600

    Create Comparison Operator

---
+++ 007_Comparison_Operator.py
@@ -0,0 +1,75 @@
+# Equal to :
+x = 55
+y = 55
+print (x==y)   # True
+
+x = 55
+y = 50
+print (x==y)  # False
+
+
+
+# NOT Equal to:
+x = 55
+y = 55
+print (x != y) # False
+
+x = 55
+y = 50
+print ( x != y)  # True
+
+
+# Greater Than :
+x = 55
+y = 50
+print (x>y)  # True
+
+x = 55
+y = 55
+print (x>y)  # False
+
+
+# Less Than :
+x = 55
+y = 50
+print (x<y)  # False
+
+x = 50
+y = 55
+print (x<y)  # True
+
+
+# Greater Than Or Equal To :
+x = 50
+y = 50
+print (x>=y)  # True
+
+x = 55
+y = 50
+print (x>=y)  # True
+
+x = 50
+y = 55
+print (x>=y)  # False
+
+
+
+# Less Than Or Equal To :
+x = 50
+y = 50
+print (x<=y) # True 
+
+x = 55
+y = 50
+print (x<=y)  # False
+
+x = 50
+y = 55
+print (x<=y)  # True
+
+
+
+"""
+There is No -- & ++ in Python.
+** Means power of N ( 2**3 = 2 Power of 3 = 8)
+"""

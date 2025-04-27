21a06711561cecd8e6dbdd9513270b77d058d689
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2025-04-28 00:22:57 +0600

    Mid Term

---
+++ Mid_2.py
@@ -0,0 +1,10 @@
+class Book:
+    def __init__(self, book_id, title, author, availability=True):
+        self.book_id=book_id
+        self.title=title
+        self.author=author
+        self.availability=availability
+
+    def __repr__(self):
+        return(f"Book(book_id={self.book_id}, title='{self.title}', "
+                f"author='{self.author}',availability={self.availability})")

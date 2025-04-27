43ef0d787f29fdd2c232cf4f3e4077cf5f59338d
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2025-04-28 00:23:22 +0600

    Mid Term

---
+++ Mid_3.py
@@ -0,0 +1,17 @@
+class Library:
+    book_list=[]
+    @classmethod
+    def entry_book(cls,book):
+        cls.book_list.append(book)
+class Book:
+    def __init__(self,book_id,title,author,availability=True):
+        self.book_id=book_id
+        self.title=title
+        self.author=author
+        self.availability=availability
+        Library.entry_book(self)
+
+    def __repr__(self):
+        return (f"Book(book_id={self.book_id}, title='{self.title}', "
+                f"author='{self.author}', availability={self.availability})")
+

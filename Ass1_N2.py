6ea178c5dae1c7e81e5a64df48f68b2efabefd4d
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2025-04-27 22:57:21 +0600

    Assignment 1

---
+++ Ass1_N2.py
@@ -0,0 +1,39 @@
+# import sys
+# input = sys.stdin.read
+# data = input().split()
+# N = 100100
+# n = int(data[0])
+# a = [0] * N
+# ans = 0
+# for i in range(1, n + 1):
+#     x = int(data[i])
+#     if x >= N:
+#         ans += 1
+#     else:
+#         a[x] += 1
+# for i in range(1, N):
+#     if a[i] < i:
+#         ans += a[i]
+#     else:
+#         ans += a[i] - i
+# print(ans)
+
+
+
+
+N=100100
+n=int(input().strip())
+a=[0]*N
+ans=0
+data=list(map(int,input().strip().split()))
+for x in data:
+    if x>=N:
+        ans+=1
+    else:
+        a[x]+=1
+for i in range(1,N):
+    if a[i]<i:
+        ans+=a[i]
+    else:
+        ans+=a[i]-i
+print(ans)

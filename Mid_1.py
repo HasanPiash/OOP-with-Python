5b2c5ac48c55ba72f1971d278f901a01e8327ea7
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2025-04-28 00:22:29 +0600

    Mid Term

---
+++ Mid_1.py
@@ -0,0 +1,18 @@
+class Library:
+    book_list=[]
+    @classmethod
+    def entry_book(cls,book):
+        cls.book_list.append(book)
+# Example usage:
+class Book:
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author
+    def __repr__(self):
+        return f"Book(title='{self.title}',author='{self.author}')"
+book1=Book("Zero To One","Peter Thiel")
+book2=Book("Atomic Habits","James Clear")
+
+Library.entry_book(book1)
+Library.entry_book(book2)
+print(Library.book_list)

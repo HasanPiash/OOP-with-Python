5977a1377d207e45abf9707757cdacb46bd09dc6
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2025-09-15 19:03:38 +0600

    Update 005_Input.py

--- 005_Input.py
+++ 005_Input.py
@@ -5,8 +5,8 @@ print(Account)
 """ 
 OUTPUT: Give me some Money:100
 RESULT: 100 
-"""
 
+"""
 First_money = input("Enter First Money: ")
 Second_money = input ("Enter Second Money: ")
 print(First_money + Second_money)
@@ -24,4 +24,6 @@ print (int (First_money) + int (Second_money))
 """ 
 INPUT: 50 & 50
 OUTPUT: 100
+
 """
+

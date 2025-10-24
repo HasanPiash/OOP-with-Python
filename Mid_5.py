c6a82115765ee3a9f02eaa896cd71af540ffb7a4
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2025-10-24 15:20:57 +0600

    Update Mid_5.py

--- Mid_5.py
+++ Mid_5.py
@@ -3,6 +3,7 @@ class Library:
     @classmethod
     def entry_book(cls,book):
         cls.book_list.append(book)
+        
 class Book:
     def __init__(self,book_id,title,author,availability=True):
         self.book_id=book_id
@@ -28,3 +29,4 @@ class Book:
     def __repr__(self):
         return (f"Book(book_id={self.book_id}, title='{self.title}', "
                 f"author='{self.author}', availability={self.availability})")
+

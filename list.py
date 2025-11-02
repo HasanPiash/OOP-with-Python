fe7bead1a7adb699ce87fe33807c1dfd924caa9f
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2025-11-02 14:57:13 +0600

    Update list.py

--- list.py
+++ list.py
@@ -3,7 +3,7 @@
 # index =   0   1   2   3   4  5   6   7   8   9  
 numbers = [45, 56, 12, 89, 87, 32, 84, 59, 46, 93]
 # index = -10  -9  -8  -7  -6  -5  -4  -3  -2  -1  
-
+ 
 print(numbers[3], numbers[-3]) 
 
 # list( start : end ) # start from the start index and stops before the end index

c1ebb6071dd1d6569ee2d58c253ef7c37e1aa500
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2025-11-16 22:49:58 +0600

    Update 002_Variable.py

--- 002_Variable.py
+++ 002_Variable.py
@@ -1,4 +1,4 @@
-age = 25
+age = 25 
 Weight = 70.5
 Name = "Hasan Piash"
 Department = "Computer Science"
@@ -9,3 +9,4 @@ print (age + Weight)
 print (Name , Department)
 
 print ('Python' + ' ' + 'Django') 
+
